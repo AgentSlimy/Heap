76d4cb57df3f3dd33d5bcf057aef356bda94659d
U-BSD\417197 <417197@SHS-10D9C06E3.beaverton.k12.or.us>
2022-03-31 11:49:06 -0700

    File input

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 /*
+Online Resources Used:
 https://en.wikipedia.org/wiki/Heap_(data_structure)
 https://en.wikipedia.org/wiki/Binary_tree
 https://www.geeksforgeeks.org/heap-using-stl-c
 https://www.geeksforgeeks.org/stack-vs-heap-memory-allocation
 https://www.geeksforgeeks.org/array-representation-of-binary-heap
-
+https://www.cplusplus.com/doc/tutorial/files/
 */
 #include <iostream>
 #include <cstring>
@@ -23,6 +24,7 @@ int main() {
   bool running = true;
   char command[10];
   char input[10000];
+  char fileName[100];
   int modify[100];
   for (int i = 0; i < 100; i++) {
     modify[i] = 0;
@@ -45,7 +47,25 @@ int main() {
       cin.clear();
       cin.ignore(10000, '\n');
       if (strcmp(command, "FILE") == 0 || strcmp(command, "File") == 0) { //File command, for file input use
-	cout << "File" << endl;
+	cout << "What is the file name? (Include the .txt) ";
+	cin.get(fileName, 100);
+	cin.clear();
+	cin.ignore(10000, '\n');
+	streampos size;
+	ifstream file(fileName, ios::in | ios::binary | ios::ate);
+	if (file.is_open()) {
+	  size = file.tellg();
+	  file.seekg(0, ios::beg);
+	  file.read(input, size);
+	  file.close();
+	  PARSE(input, modify, count);
+	  cout << "Input: ";
+	  for (int i = 0; i < 100; i++) {
+	    if (modify[i] == 0) break;
+	    cout << modify[i] << " ";
+	  }
+	  cout << endl;
+	} 
       }
       else if (strcmp(command, "MANUAL") == 0 || strcmp(command, "Manual") == 0) { //Manual command, for user manual input
 	cout << "Enter each number followed by a space:" << endl;
